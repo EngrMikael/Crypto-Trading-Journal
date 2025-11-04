3ced120bd57440442345580e475b0918eadef71e
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-04 20:02:17 +0800

    re configured my directory and adjusted my databse.py

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,11 +1,36 @@
-from typing import Annotated
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+# backend/app/core/database.py
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 from contextlib import contextmanager
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+dotenv_path = os.path.join(BASE_DIR, ".env")
 
-DATABASE_URL = os.getenv("no_info_yet")
+print(f"Looking for .env at: {dotenv_path}")
+print(f".env exists: {os.path.exists(dotenv_path)}")
+
+load_dotenv(dotenv_path)
+
+# PostgreSQL connection string from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(f"DATABASE_URL loaded: {DATABASE_URL}")
 
 engine = create_engine(DATABASE_URL, echo = True)
+
+
+class User(SQLModel, table = True):
+    __tablename__ = "users"  # type: ignore
+    __table_args__ = {"schema": "auth"}
+    id: int | None = Field(default = None, primary_key = True)
+    email: str = Field(unique = True, index = True)
+    hashed_password: str
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+@contextmanager
+def get_session():
+    with Session(engine) as Session:
+        yield session
