1d53d009427e8b704ba5e92b2e4d025ef7b4e881
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-04 17:09:39 +0800

    adjusted my main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
 # this will be the main script for my backend
+# this main.py as i learned will be my entire app, 
+# it will handle everything, 
+# for now i will handle my ORM/database
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
