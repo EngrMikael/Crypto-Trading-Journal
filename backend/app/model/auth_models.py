fe7d826a62f37ab3a2e06da00dcf95d6bdbf7c91
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-07 19:59:32 +0800

    new update for auth_models

--- backend/app/model/auth_models.py
+++ backend/app/model/auth_models.py
@@ -3,3 +3,15 @@ from sqlmodel import SQLModel
 class UserCreate(SQLModel):
     email : str
     password : str
+    
+class UserRead(SQLModel):
+    id: int
+    email: str
+
+class UserLogin(SQLModel):
+    email: str
+    password: str
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str = "bearer"
