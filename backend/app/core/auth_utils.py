319b0310c46feb520336e15edca37e4493ca2e32
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-08 20:32:13 +0800

    updated utils with hashing, and expiry token

--- backend/app/core/auth_utils.py
+++ backend/app/core/auth_utils.py
@@ -1,3 +1,5 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
@@ -18,16 +20,36 @@ print("Secret Key loaded")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes = ["bcrypt"], depracated = "auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/api/login")
 
-def hash_password(password: str):
+pwd_context = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
+
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(password: str, hashed_password: str):
-    return pwd_context.hash(password, hashed_password)
+def verify_password(password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(password, hashed_password)
 
-def create_access_token(data: dict, expires_delta: int | None = None):
+def create_access_token(data: dict, expires_delta: int | None = None) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes = expires_delta or ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
+
+def get_current_user(token: str = Depends(oauth2_scheme)) -> int:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str | None = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid authentication token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return int(user_id)
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
