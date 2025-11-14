e91650330ea151087f4898f41bf7f9150ab94459
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-14 12:53:42 +0800

    added a prototype dashboard with a button to trades, because navbar is not yet implemented

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Dashboard() {
+    const navigate = useNavigate();
+    
+    const goToJournal = () => {
+        navigate("/journal");
+    }
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+      <button 
+        type="button" 
+        className="bg-blue-500 text-white px-4 py-2 mt-4"
+        onClick={goToJournal} // add click handler
+      >
+        My Trades
+      </button>
+    </div>
+
+  );
+}
