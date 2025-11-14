09042f0945d5b1201b9a7fa2999019e48dd91ede
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-14 11:45:21 +0800

    json files including api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
-
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
