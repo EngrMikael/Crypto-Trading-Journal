7ad1b76079f96cbfc1acefc12c700b7c31e2d045
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-16 19:41:06 +0800

    Applied navbar to a simple prototype page for settings

---
+++ frontend/src/pages/Settings.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Settings() {
+  return (
+    <div className="p-4">
+      <Navbar />
+      <h1 className="text-2xl font-bold mt-6">Settings</h1>
+      <p>Modify your account settings here.</p>
+    </div>
+  );
+}
