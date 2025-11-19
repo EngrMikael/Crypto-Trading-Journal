a11046f529e04fe308e4029b10c1a509358ccd22
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-19 19:19:36 +0800

    updated the App for the ProtectedRoutes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,60 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Journal from "./pages/Journal";
 import Dashboard from "./pages/Dashboard";
-import Register from "./pages/Register"
-import RegisterTrade from "./pages/RegisterTrade";
 import Settings from "./pages/Settings";
+import RegisterTrade from "./pages/RegisterTrade";
+import TradeForm from "./components/TradeForm";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/journal" element={<Journal />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/register/trade" element={<RegisterTrade />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* new */}
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/journal"
+            element={
+              <ProtectedRoutes>
+                <Journal />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoutes>
+                <Dashboard />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/settings"
+            element={
+              <ProtectedRoutes>
+                <Settings />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/register/trade"
+            element={
+              <ProtectedRoutes>
+                <TradeForm />
+              </ProtectedRoutes>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
