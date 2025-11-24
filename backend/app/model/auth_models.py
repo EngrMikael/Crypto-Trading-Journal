bc929ebe7e739b0875d26e7e2375312d7e578f7c
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-11-24 18:53:55 +0800

    contemplating whether to add a username and make it optional in the UserCreate function

--- backend/app/model/auth_models.py
+++ backend/app/model/auth_models.py
@@ -1,11 +1,14 @@
 from sqlmodel import SQLModel
 
 class UserCreate(SQLModel):
+    # I'm not sure if the username statiate is necessary for user create since it is optional in registering a new user, 
+    # but can be edited once inside the user settings/dashboard
     email : str
     password : str
     
 class UserRead(SQLModel):
     id: int
+    username: str
     email: str
 
 class UserLogin(SQLModel):
