7402bffc509c2f2fd52bc5ea4c2569921c620cbd
EngrMikael <prof.engr.tadeomj@gmail.com>
2025-12-07 20:08:52 +0800

    an optional update for auth_models.py to be reviewed later once the  programming restarts, currently in code review mode. to deepen understanding for FastAPI and ORMs

--- backend/app/model/auth_models.py
+++ backend/app/model/auth_models.py
@@ -19,3 +19,29 @@ class UserLogin(SQLModel):
 class Token(SQLModel):
     access_token: str
     token_type: str = "bearer"
+
+
+# apply this later to see if it breaks or not
+# a faster, cleaner, and confusion free code
+
+# from pydantic import BaseModel
+
+# class UserCreate(BaseModel):
+#     # I decided to add a username but make it optional
+#     email : str
+#     username : str | None = None
+#     password : str
+    
+# class UserRead(BaseModel):
+#     # I need to make the UserRead None username otherwise it will raise an error
+#     id: int
+#     username: str | None = None
+#     email: str
+
+# class UserLogin(BaseModel):
+#     email: str
+#     password: str
+
+# class Token(BaseModel):
+#     access_token: str
+#     token_type: str = "bearer"
